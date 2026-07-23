ceb123ffda3106b3967250f05e7bbe498abd762f
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2026-07-23 15:08:46 +0100

    builtIn middlewares

---
+++ 6. Advanced FastAPI Concepts/middleware/gzip-middleware.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from starlette.middleware.gzip import GZipMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size= 1000
+) 
+
+# Define Your Endpoints
+
+
---
+++ 6. Advanced FastAPI Concepts/middleware/https-middleware.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+
+app = FastAPI()
+
+app.add_middleware(HTTPSRedirectMiddleware)
+
+
+# Define Your Endpoints
+
